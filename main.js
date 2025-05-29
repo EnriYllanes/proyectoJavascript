eb0335c077908b1d3b9ca5415ce2381b99310463
EnriYllanes <Enri.Yllanes@gmail.com>
2025-05-29 18:29:29 -0300

    proyectojs

--- main.js
+++ main.js
@@ -1,63 +1,3 @@
-//Semana y Clase 3
-//Simula un login con usuario y contraseña predefinidos.
-
-const userDB = "45678343"
-const passDB = "1235"
-
-let error = alert ("error 404")
-let username = prompt ("Cual es tu user")
-let contraseña = prompt ("Cual es tu contraseña")
-
-
-
-let credencial = login(username, contraseña)
-
-function login(usuario, pass){
-    let cantidadDeIntentos = 3
-    let ingreso = false
-
-    for( let i = 0; i < cantidadDeIntentos; i++) {
-    if(usuario == null || pass == null){
-        alert("O la contraseña o el usuario estan vacios")
-        
-    }else{
-            if(userDB === usuario && passDB === pass){
-                alert("!!!Entraste!!!")
-                ingreso = true
-                return ingreso
-
-            }else if(userDB === usuario && passDB != pass){
-                alert("Tu contraseña es incorrecta")
-
-            }else if(userDB != usuario && passDB === pass){
-                alert("Tu usuario es incorrecto")
-
-            }else{
-                alert("!Ta todo mal!")
-            }
-            usuario = prompt("Cual es tu user")
-            pass = prompt ("Cual es tu contraseña")
-        }
-    }
-
-    alert("Ahi va la polesia a tu casa")
-    return false
-}
-
-
-
-
-if(credencial){
-
-    //ingreso === false
-    if(!ingreso){
-        alert("No entraste, ya te mande a la polesia")
-    }
-
-    //Ingreso === true
-    if(ingreso){
-
-}
 
 let saldoUser = Number(prompt("¿Cuanta plata tiene en el banco?"))
 
@@ -84,16 +24,12 @@ while(bandera){
                 alert("No puede ingresas valores negativos")
             }
         break
-
         case 2:
             alert(saldo)
         break
-
         case 3:
             let montoRetirar = Number(prompt("¿Cuanto quiere sacar?"))
-
             if(montoRetirar > 0){//Que no sea negativo
-                
                 if(montoRetirar > saldo){//Que no quiera rettirar mas plata que la que tiene
                     alert("no podes retirar mas dinero que el que tienes...")
                 }else{
@@ -103,7 +39,6 @@ while(bandera){
                 alert("¿Que te crees?")
             }
         break
-
         case 4:
             let alias = prompt("¿Cual es el alias de la persona a transferir?")
             if(alias == null || alias.length <= 8){
@@ -154,6 +89,4 @@ while(bandera){
     }else{
         alert("Tiene que ser un numero")
     }
-}
-
 }
